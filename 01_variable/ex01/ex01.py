9a5c9408e7de7b7c01d7cc187cbd28774c4d63b3
marobiana <69413030+marobiana@users.noreply.github.com>
2021-01-27 15:47:43 +0900

    Update ex01.py

---
+++ 01_variable/ex01/ex01.py
@@ -0,0 +1,34 @@
+# 주석 : 개발자가 보기 위한 설명. 프로그램 수행시 해석되지 않는 코드
+# 한줄주석
+"""
+문장
+문장2
+문장3
+"""
+
+'''
+여러줄 주석
+주석
+'''
+
+print(3+5)
+
+print("Hello world!")
+
+print('Hello world')
+
+print("She's gone.")
+print("문장",12333)
+
+
+print(3 + 5)
+
+
+# 문자열 출력
+print("hello world")
+
+
+# 한 줄로 쓰려면
+# 두 문자열 사이에 공백이 들어감
+print("She's", "gone")
+print("text", 1234)
