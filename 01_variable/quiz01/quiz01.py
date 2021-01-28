821064116a14bfe5aa675e6cf22e5f61e90c55f0
marobiana <69413030+marobiana@users.noreply.github.com>
2021-01-28 11:51:04 +0900

    Update quiz01.py

--- 01_variable/quiz01/quiz01.py
+++ 01_variable/quiz01/quiz01.py
@@ -12,7 +12,7 @@
 name = '신보람'
 age = 25
 print("이름:", name)
-print("나이:", age, "\n")
+print("나이:", age, "\n")  # "\n"  줄바꿈
 
 num1 = 3
 num2 = 5
@@ -21,7 +21,7 @@ result = num1 + num2
 print(num1, "+", num2, "=", result)
 # 3 - 5 = -2
 print(num1, '-', num2, '=', (num1 - num2))
-print()
+print()  # 줄바꿈
 
 # 7 X 1 = 7
 num = 7
