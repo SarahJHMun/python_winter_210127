c4cbcc8f4d03bca0909b9fd345b6e87caeb7b6d6
marobiana <69413030+marobiana@users.noreply.github.com>
2021-01-28 11:50:36 +0900

    Update quiz01.py

---
+++ 01_variable/quiz01/quiz01.py
@@ -0,0 +1,32 @@
+# 이름: 신보람
+# 나이: 25
+#
+# 3 + 5 = 8
+# 3 - 5 = -2
+#
+# 7 X 1 = 7
+# 7 X 2 = 14
+# 7 X 3 = 21
+# 7 X 4 = 28
+# 7 X 5 = 35
+name = '신보람'
+age = 25
+print("이름:", name)
+print("나이:", age, "\n")
+
+num1 = 3
+num2 = 5
+result = num1 + num2
+# 3 + 5 = 8
+print(num1, "+", num2, "=", result)
+# 3 - 5 = -2
+print(num1, '-', num2, '=', (num1 - num2))
+print()
+
+# 7 X 1 = 7
+num = 7
+print(num, 'X', 1, '=', (num * 1))
+print(num, 'X', 2, '=', (num * 2))
+print(num, 'X', 3, '=', (num * 3))
+print(num, 'X', 4, '=', (num * 4))
+print(num, 'X', 5, '=', (num * 5))
