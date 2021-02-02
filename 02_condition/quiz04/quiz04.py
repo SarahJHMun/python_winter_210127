fb0ac9be5c531298df01e39a5065f4b84d3b0ed3
marobiana <69413030+marobiana@users.noreply.github.com>
2021-02-02 12:49:53 +0900

    Update quiz04.py

--- 02_condition/quiz04/quiz04.py
+++ 02_condition/quiz04/quiz04.py
@@ -1,40 +1,40 @@
 # 1. 두 점수를 입력 받고, 평균이 70점 이상이면 합격 그렇지 않으면 불합격을 출력하세요.
-# score1, score2 = input("두 점수를 입력하세요:").split()
-# score1 = int(score1)
-# score2 = int(score2)
-# average = (score1 + score2) / 2
-# if average >= 70:
-#     print("합격")
-# else:
-#     print("불합격")
+score1, score2 = input("두 점수를 입력하세요:").split()
+score1 = int(score1)
+score2 = int(score2)
+average = (score1 + score2) / 2
+if average >= 70:
+    print("합격")
+else:
+    print("불합격")
 
 # 2. 세 개의 정수를 입력 받아서 가장 큰 값을 출력하세요.
-# a, b, c = input("세 개의 정수를 입력하세요:").split()
-# a = int(a)   # 1
-# b = int(b)   # 3
-# c = int(c)   # 5
-#
-# max = a
-# if max < b:
-#     max = b
-# if max < c:
-#     max = c
-# print(max)
+a, b, c = input("세 개의 정수를 입력하세요:").split()
+a = int(a)   # 1
+b = int(b)   # 3
+c = int(c)   # 5
+
+max = a
+if max < b:
+    max = b
+if max < c:
+    max = c
+print(max)
 
 # 3.
 # 평균이 60점 이상이면 합격
 #   한과목이라도 50점 이하면 무조건 과락
 # 평균 60점 미만이면 불합격
-# score1 = int(input("점수1:"))
-# score2 = int(input("점수2:"))
-# average = (score1 + score2) / 2
-# if average < 60:
-#     print("불합격")
-# else:  # 평균 60점 이상
-#     if score1 <= 50 or score2 <= 50:
-#         print("과락")
-#     else:
-#         print("합격")
+score1 = int(input("점수1:"))
+score2 = int(input("점수2:"))
+average = (score1 + score2) / 2
+if average < 60:
+    print("불합격")
+else:  # 평균 60점 이상
+    if score1 <= 50 or score2 <= 50:
+        print("과락")
+    else:
+        print("합격")
 
 # 4로 나누어 떨어지는 연도는 윤년이다.
 # 100으로 나누어 떨어지는 연도는 윤년이 아니다.
